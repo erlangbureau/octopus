dca4a3d62ff3964e5b24362f8e4db2b5c22eef18
Serhii Kostiushkin <s.kostyushkin@gmail.com>
2019-05-07 05:09:29 +0300

    Switch pool_sup specs to map

--- src/octopus_pool_sup.erl
+++ src/octopus_pool_sup.erl
@@ -18,24 +18,31 @@ start_link(PoolId) ->
 
 
 %% supervisor callbacks
--spec init([PoolId]) -> {ok, {{Strategy, MaxR, MaxT}, [ChildSpec]}}
-when
-    PoolId      :: atom(),
-    Strategy    :: supervisor:strategy(),
-    MaxR        :: non_neg_integer(),
-    MaxT        :: pos_integer(),
-    ChildSpec   :: supervisor:child_spec().
-
 init([PoolId]) ->
     Procs = [
-        {octopus_pool_workers_sup,
-            {octopus_pool_workers_sup, start_link, [PoolId]},
-            transient, infinity, supervisor, [octopus_pool_workers_sup]},
-        {octopus_pool_task_server,
-            {octopus_pool_task_server, start_link, [PoolId]},
-            transient, 1000, worker, [octopus_pool_task_server]},
-        {octopus_pool_config_server,
-            {octopus_pool_config_server, start_link, [PoolId]},
-            transient, 1000, worker, [octopus_pool_config_server]}
+        #{
+            id          => octopus_pool_workers_sup,
+            start       => {octopus_pool_workers_sup, start_link, [PoolId]},
+            restart     => transient,
+            shutdown    => infinity,
+            type        => supervisor,
+            modules     => [octopus_pool_workers_sup]
+        },
+        #{
+            id          => octopus_pool_task_server,
+            start       => {octopus_pool_task_server, start_link, [PoolId]},
+            restart     => transient,
+            shutdown    => 1000,
+            type        => worker,
+            modules     => [octopus_pool_task_server]
+        },
+        #{
+            id          => octopus_pool_config_server,
+            start       => {octopus_pool_config_server, start_link, [PoolId]},
+            restart     => transient,
+            shutdown    => 1000,
+            type        => worker,
+            modules     => [octopus_pool_config_server]
+        }
     ],
     {ok, {{one_for_all, 5, 1}, Procs}}.
